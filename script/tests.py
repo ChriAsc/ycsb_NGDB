02472e9ef2cff0c5f0b6bd2b14c99ff5643245be
ChriAsc <ascanic.98@gmail.com>
2023-09-21 22:41:44 +0200

    edit start command

--- script/tests.py
+++ script/tests.py
@@ -12,10 +12,14 @@ def main(database, workload, op_counts):
         run_command = f"../ycsb-{database}-binding-0.18.0-SNAPSHOT/bin/ycsb.sh run {database} -s -P ../workloads/workload{workload} -p mongodb.url=mongodb://127.0.0.1:27017/ycsb?retryWrites=true "
         cleanup_command = "mongosh < mongoclean.mongodb"
     elif database == "cassandra":
+        load_command_start = f""
+        run_command_start = f""
         load_command = f"../ycsb-{database}-binding-0.18.0-SNAPSHOT/bin/ycsb.sh load {database}2-cql -s -P ../workloads/workload{workload} -p hosts=127.0.0.1"
         run_command = f""
         cleanup_command = f""
     elif database == "redis":
+        load_command_start = f"../ycsb-{database}-binding-0.18.0-SNAPSHOT/bin/ycsb.sh load {database} -s -P workloads/workload{workload} -p \"redis.host=127.0.0.1\" -p \"redis.port=6379\""
+        run_command_start = f"../ycsb-{database}-binding-0.18.0-SNAPSHOT/bin/ycsb.sh run {database} -P workloads/workload{workload} -p \"redis.host=127.0.0.1\" -p \"redis.port=6379\""
         load_command = f"../ycsb-{database}-binding-0.18.0-SNAPSHOT/bin/ycsb.sh load {database} -s -P workloads/workload{workload} -p \"redis.host=127.0.0.1\" -p \"redis.port=6379\""
         run_command = f"../ycsb-{database}-binding-0.18.0-SNAPSHOT/bin/ycsb.sh run {database} -P workloads/workload{workload} -p \"redis.host=127.0.0.1\" -p \"redis.port=6379\""
         cleanup_command = "docker exec -it redis-container redis-cli FLUSHALL"
